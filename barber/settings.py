7b347e05c154c0ef77bf094bd6304a9a3acdb872
vladovichsvetlana <107796276+vladovichsvetlana@users.noreply.github.com>
2023-03-29 12:33:46 +0300

    add in settings django-heroku

--- barber/settings.py
+++ barber/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-!09u#th&^490x77&x4miux0d@-3!=x(74xdjrmct!e)1pkd@_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -156,3 +156,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+import django_heroku
+django_heroku.settings(locals())
